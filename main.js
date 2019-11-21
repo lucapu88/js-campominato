613ab1367e70edf4c7ff05a2feb478a565872b02
lucapu88 <caputoluca88@gmail.com>
2019-11-21 18:32:56 +0100

    mi mancano alcuni punti, che oggi, per mancanza di tempo non riesco a completare :(

--- main.js
+++ main.js
@@ -6,22 +6,6 @@
 // BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali.
 // Con difficoltà 0=> tra 1 e 100, con difficoltà 1 =>  tra 1 e 80, con difficoltà 2=> tra 1 e 50
 
-// 1) Scrivere la consegna
-// 2) Scomporre il problema in punti
-// 3) Espandere i punti BANALI
-// 4) Tradurre in codice
-//-------------------------------
-// Il computer deve generare 16 numeri casuali tra 1 e 100 (queste saranno le mine).
-// 1) creo l’array(vuoto)
-// 2)Creo un numero causale
-// 3)Controllo che il numero non ci sia nell’Array
-// 4)Se non c’è lo metto nell’array
-// In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// Se il numero è presente nella lista dei numeri generati, la partita termina (l’utente ha beccato una mina), altrimenti si continua chiedendo all’utente un altro numero.
-// 1) Se pesto la mina GAME OVER(se il numero è nell’array sei al CREATORE )
-// 2)Altrimenti ripetiamo la richiesta di inserire un numero
-// La partita termina quando il giocatore inserisce un numero “mina” o raggiunge il numero massimo possibile di numeri consentiti (84).
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 // creo l’array(vuoto)
 var mineGenerate = [];
@@ -34,12 +18,15 @@ while (mineGenerate.length < 16) {
  }
 }
 
-do {
+do { //In seguito chiedo all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
   var numero = parseInt(prompt('Digita un numero da 1 a 100'));
   console.log(mineGenerate.includes(numero));
-} while (!mineGenerate.includes(numero));
-
+} while (!mineGenerate.includes(numero));// Se il numero è presente nella lista dei numeri generati, la partita termina
   console.log('BOOOOOOOOM!!! hai perso!!! sei saltato in aria in 1000 pezzi');
+
+  //ma quando vince?
+  //se digita sempre lo stesso numero che non è nell'array?
+  //se digita una lettera?
 
 //funzioni
 function generaRandom(min, max) {
