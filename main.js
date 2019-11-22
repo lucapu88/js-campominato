9d2a169323420d8f052c7622f5d9eed42e7e18a1
lucapu88 <caputoluca88@gmail.com>
2019-11-22 14:56:25 +0100

    mi manca: 1-aprire un alert nel caso l'utente digita cose errate. 2-far uscire dal ciclo nel caso di vittoria dell'utente.

--- main.js
+++ main.js
@@ -14,27 +14,31 @@ while (mineGenerate.length < 16) {
  var mine = generaRandom(1, 100); //Creo un numero causale
  if(!mineGenerate.includes(mine)) { //Controllo che il numero non ci sia nell’Array
        mineGenerate.push(mine); //Se non c’è lo metto nell’array (mineGenerate)
-       console.log('mine' + mineGenerate);
+       console.log('mine ' + mineGenerate);
  }
  contatore++
 }
 
-var numeriBuoni = [];
+var numeriBuoni = []
 do { //In seguito chiedo all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
   var numero = parseInt(prompt('Digita un numero da 1 a 100'));
-  if (numero!= mineGenerate) {//i numeri digitati dall'utente vanno in un altro array (numeriBuoni) se sono diversi da quelli inseriti nell'array mineGenerate
+  if (numero != mineGenerate && !numeriBuoni.includes(numero)) {//i numeri digitati dall'utente vanno in un altro array (numeriBuoni) se sono diversi da quelli inseriti nell'array mineGenerate
     numeriBuoni.push(numero);
+  } else if (numeriBuoni.includes(numero) && isNaN(numero) && numero < 1 && numero > 100) { //controllo che l'utente abbia inserito un numero corretto
+    console.log('numero non corretto');
+    alert('O stai cercando di barare, o hai digitato qualcosa che non è un numero!');
   }
-   console.log('numeri nuoni' + numeriBuoni);
-   if (numeriBuoni.length == 84) {//se l'utente arriva al numero max di tentativi senza beccare mine ha vinto
-     console.log('CHE CULO!!! hai vinto :-P');
+   console.log('numeri buoni ' + numeriBuoni);
+   if (numeriBuoni.length == 5) {//se l'utente arriva al numero max di tentativi senza beccare mine ha vinto
+     console.log(':-P You Win...CHE CULOOOOO!!!!');
+
    }
 
 } while (!mineGenerate.includes(numero)) {// Se il numero è presente nella lista delle mine generate, la partita termina
-  console.log('BOOOOOOOOM!!! hai perso!!! sei saltato in aria in 1000 pezzi');
+  console.log('BOOOOOOOOM!!! GAME OVER!!! sei saltato in aria in 1000 pezzi');
 }
 
-  //se digita sempre lo stesso numero che non è nell'array?
+
   //se digita una lettera?
 
 //funzioni
